0160fa1d9a761ee6c662ff1173ba9fdfc10a7603
Aaron Dodd <contact@aaronrdodd.com>
2023-03-29 10:39:32 +0100

    Solve valid parentheses
    Signed-off-by: Aaron Dodd <contact@aaronrdodd.com>

---
+++ CompetitiveProgramming/ValidParenthesesTests.cs
@@ -0,0 +1,47 @@
+﻿namespace CompetitiveProgramming;
+
+public class ValidParenthesesTests
+{
+    private static bool ValidParentheses(string s)
+    {
+        var stack = new Stack<char>();
+        var pairs = new Dictionary<char, char>()
+        {
+            [')'] = '(',
+            [']'] = '[',
+            ['}'] = '{',
+        };
+
+        foreach (var c in s)
+        {
+            if (!pairs.ContainsKey(c) && !pairs.ContainsValue(c))
+            {
+                return false;
+            }
+
+            if (!pairs.ContainsKey(c))
+            {
+                stack.Push(c);
+            }
+            else if (stack.Count == 0 || stack.Pop() != pairs[c])
+            {
+                return false;
+            }
+        }
+
+        return stack.Count == 0;
+    }
+
+    [Theory]
+    [InlineData("()", true)]
+    [InlineData("()[]{}", true)]
+    [InlineData("((){})", true)]
+    [InlineData("(]", false)]
+    [InlineData("(defun x (x y) (* x y))", false)]
+    [InlineData("((){}", false)]
+    public void ValidParentheses_Tests(string s, bool expected)
+    {
+        var actual = ValidParentheses(s);
+        Assert.Equal(expected, actual);
+    }
+}
