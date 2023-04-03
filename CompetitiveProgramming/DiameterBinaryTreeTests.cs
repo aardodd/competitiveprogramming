ef83011f9dcafe4668a79f55d04acac457a0cfa3
Aaron Dodd <contact@aaronrdodd.com>
2023-04-03 10:44:04 +0100

    Solve diameter of binary tree
    Signed-off-by: Aaron Dodd <contact@aaronrdodd.com>

---
+++ CompetitiveProgramming/DiameterBinaryTreeTests.cs
@@ -0,0 +1,37 @@
+﻿namespace CompetitiveProgramming;
+
+public class DiameterBinaryTreeTests
+{
+    private static int DiameterBinaryTree(TreeNode root)
+    {
+        return DepthFirstSearch(root).Item2;
+    }
+
+    private static (int, int) DepthFirstSearch(TreeNode? root, int maxDepth = 0)
+    {
+        if (root == null)
+        {
+            return (-1, maxDepth);
+        }
+
+        var left = 1 + DepthFirstSearch(root.Left).Item1;
+        var right = 1 + DepthFirstSearch(root.Right).Item1;
+
+        maxDepth = Math.Max(maxDepth, left + right);
+        
+        return (Math.Max(left, right), maxDepth);
+    }
+
+    [Fact]
+    public void DiameterBinaryTree_GivenValidTree_ReturnsDiameter()
+    {
+        // Arrange
+        var input = new TreeNode(1, new TreeNode(2, new TreeNode(4), new TreeNode(5)), new TreeNode(3));
+
+        // Act
+        var actual = DiameterBinaryTree(input);
+
+        // Assert
+        actual.Should().Be(3);
+    }
+}
